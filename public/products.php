562aefa0f35b54daf720fd332812de65adb723fe
Mario Rocha <mrocha51248@gmail.com>
2021-10-18 11:17:02 +0200

    delete products.php and render in index.php instead

--- public/products.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/twig.php';
-
-$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre'];
-
-echo $twig->render('products.html.twig', ['products' => $products]);
