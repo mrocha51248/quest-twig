1031469d9a748b11d90b819ccd760d9fc6a3e870
Mario Rocha <mrocha51248@gmail.com>
2021-10-18 11:13:55 +0200

    Implement products controller and view

---
+++ public/products.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/../config/twig.php';
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre'];
+
+echo $twig->render('products.html.twig', ['products' => $products]);
